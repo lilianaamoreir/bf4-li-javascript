a08f56c86b6276c5056c72b34733e9643de4617b
lilianaamoreir <moreiralili13@gmail.com>
2025-05-14 16:16:52 +0100

    Add files via upload

---
+++ JavaScript/exer.js
@@ -0,0 +1,32 @@
+let a = 10;
+let b = 5;
+
+let tmp = a;
+a = b;
+b = tmp;
+
+console.log(a, b);
+
+//ordenar array
+
+let arr = [131, 312, 234, 12, 7, 45, 234];
+
+arr.sort((a, b) => a - b);
+a = 5;
+b = 10; 
+
+console.log(arr);
+
+//21/blackjack
+
+/*const BLACKJACK = 21;
+
+let jogadorA = 0;
+let jogadorB = 0;*/
+
+//21/blackjack
+let cups = ["", "", ""];
+
+let randomIndex = Math.floor(Math.random() * 3);
+cups [ballPosition] = "0";
+console.log()
